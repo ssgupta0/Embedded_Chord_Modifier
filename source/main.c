2e6de73e889ef71f91e4337041358879e7ac8188
Shashvat Gupta <45471982+ssgupta0@users.noreply.github.com>
2021-06-03 20:54:47 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -16,55 +16,40 @@
 
 
 
-//PB7
-void set_PWM1(double frequency) {
+//PB3
+void set_PWM0(double frequency) {
     static double current_frequency;
 
     if (frequency != current_frequency) {
-//         if(!frequency)
-//             TCCR3B &= 0x08;
-//         else
-            TCCR3B |= 0x03;
-
-//         if(frequency < 0.954)
-//             OCR3B = 0xFFFF;
-//         else if (frequency > 31250)
-//             OCR3B = 0x0000;
-//         else
-            OCR3A = (short) (8000000 / (128 * frequency)) - 1;
+            TCCR0B |= 0x03;
+	    
+            OCR0A = (short) (8000000 / (128 * frequency)) - 1;
         
-        TCNT3 = 0;
+        TCNT0 = 0;
         current_frequency = frequency;
    }
 }
 
-void PWM_on1() {
-    TCCR3A = (1 << COM3A0);
-    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
-    set_PWM1(0);
+void PWM_on0() {
+    TCCR0A = (1 << COM0A0);
+    TCCR0B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
+    set_PWM0(0);
 }
 
-void PWM_off1() {
-    TCCR3A = 0x00;
-    TCCR3B = 0x00;
+void PWM_off0() {
+    TCCR0A = 0x00;
+    TCCR0B = 0x00;
 }
 
+//----------------------
 
-//PB6
-void set_PWM2(double frequency) {
+//PB3
+void set_PWM1(double frequency) {
     static double current_frequency;
 
     if (frequency != current_frequency) {
-//         if(!frequency)
-//             TCCR3B &= 0x08;
-//         else
-            TCCR3B |= 0x03;
-
-//         if(frequency < 0.954)
-//             OCR3A = 0xFFFF;
-//         else if (frequency > 31250)
-//             OCR3A = 0x0000;
-//         else
+            TCCR1B |= 0x03;
+	    
             OCR1A = (short) (8000000 / (128 * frequency)) - 1;
         
         TCNT1 = 0;
@@ -72,33 +57,26 @@ void set_PWM2(double frequency) {
    }
 }
 
-void PWM_on2() {
+void PWM_on1() {
     TCCR1A = (1 << COM1A0);
     TCCR1B = (1 << WGM12) | (1 << CS11) | (1 << CS10);
-    set_PWM2(0);
+    set_PWM1(0);
 }
 
-void PWM_off2() {
+void PWM_off1() {
     TCCR1A = 0x00;
     TCCR1B = 0x00;
 }
 
+//----------------------
 
-//PD7
-void set_PWM3(double frequency) {
+//PB3
+void set_PWM2(double frequency) {
     static double current_frequency;
 
     if (frequency != current_frequency) {
-//         if(!frequency)
-//             TCCR3B &= 0x08;
-//         else
-            TCCR3B |= 0x03;
-
-//         if(frequency < 0.954)
-//             OCR2A = 0xFFFF;
-//         else if (frequency > 31250)
-//             OCR2A = 0x0000;
-//         else
+            TCCR2B |= 0x03;
+	    
             OCR2A = (short) (8000000 / (128 * frequency)) - 1;
         
         TCNT2 = 0;
@@ -106,50 +84,43 @@ void set_PWM3(double frequency) {
    }
 }
 
-void PWM_on3() {
-    TCCR2A = (1 << COM2B1);
+void PWM_on0() {
+    TCCR2A = (1 << COM2A0);
     TCCR2B = (1 << WGM22) | (1 << CS21) | (1 << CS20);
-    set_PWM3(0);
+    set_PWM0(0);
+}
+
+void PWM_off2() {
+    TCCR2A = 0x00;
+    TCCR2B = 0x00;
+}
+
+//----------------------
+
+//PB3
+void set_PWM3(double frequency) {
+    static double current_frequency;
+
+    if (frequency != current_frequency) {
+            TCCR3B |= 0x03;
+	    
+            OCR3A = (short) (8000000 / (128 * frequency)) - 1;
+        
+        TCNT3 = 0;
+        current_frequency = frequency;
+   }
+}
+
+void PWM_on3() {
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM0(0);
 }
 
 void PWM_off3() {
     TCCR3A = 0x00;
     TCCR3B = 0x00;
 }
-
-//PD6
-void set_PWM4(double frequency) {
-    static double current_frequency;
-
-    if (frequency != current_frequency) {
-//         if(!frequency)
-//             TCCR2B &= 0x08;
-//         else
-            TCCR2B |= 0x03;
-
-//         if(frequency < 0.954)
-//             OCR2B = 0xFFFF;
-//         else if (frequency > 31250)
-//             OCR2B = 0x0000;
-//         else
-            OCR2B = (short) (8000000 / (128 * frequency)) - 1;
-        
-        TCNT2 = 0;
-        current_frequency = frequency;
-   }
-}
-
-void PWM_on4() {
-    TCCR2A = (1 << COM2A0);
-    TCCR2B = (1 << WGM21) | (1 << CS21) | (1 << CS20);
-    set_PWM4(0);
-}
-
-void PWM_off4() {
-    TCCR2A = 0x00;
-    TCCR2B = 0x00;
-}
-
 /**/
 
 
@@ -163,10 +134,10 @@ int main(void) {
 // 	TimerOn();
 	
 		
+	PWM_on0();
 	PWM_on1();
 	PWM_on2();
 	PWM_on3();
-	PWM_on4();
     /* Insert your solution below */
     
     	
@@ -175,17 +146,17 @@ int main(void) {
     while (1) {
 // 	while(!TimerFlag);
 	    //note();
-	    set_PWM1(440);
-	    set_PWM2(554.37);
-	    set_PWM3(659.25);
-	    set_PWM4(880);
+	    set_PWM0(440);
+	    set_PWM1(554.37);
+	    set_PWM2(659.25);
+	    set_PWM3(880);
 	    
 // 	    TimerFlag = 0;
     }
-  	PWM_off1();
-   	PWM_off2();
-    	PWM_off3();
-   	PWM_off4();
+  	PWM_off0();
+   	PWM_off1();
+    	PWM_off2();
+   	PWM_off3();
     
 	return 1;
 }
