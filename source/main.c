c1fb2e098745d43b929d935b9717504ecb2deeef
Shashvat Gupta <45471982+ssgupta0@users.noreply.github.com>
2021-06-03 20:40:22 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -54,33 +54,33 @@ void PWM_off1() {
 void set_PWM2(double frequency) {
     static double current_frequency;
 
-    if (frequency != current_frequency) {
-        if(!frequency)
-            TCCR3B &= 0x08;
-        else
+//     if (frequency != current_frequency) {
+//         if(!frequency)
+//             TCCR3B &= 0x08;
+//         else
             TCCR3B |= 0x03;
 
-        if(frequency < 0.954)
-            OCR3A = 0xFFFF;
-        else if (frequency > 31250)
-            OCR3A = 0x0000;
-        else
-            OCR3A = (short) (8000000 / (128 * frequency)) - 1;
+//         if(frequency < 0.954)
+//             OCR3A = 0xFFFF;
+//         else if (frequency > 31250)
+//             OCR3A = 0x0000;
+//         else
+            OCR1A = (short) (8000000 / (128 * frequency)) - 1;
         
-        TCNT3 = 0;
+        TCNT1 = 0;
         current_frequency = frequency;
    }
 }
 
 void PWM_on2() {
-    TCCR3A = (1 << COM3A0);
-    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    TCCR1A = (1 << COM1A0);
+    TCCR1B = (1 << WGM12) | (1 << CS11) | (1 << CS10);
     set_PWM2(0);
 }
 
 void PWM_off2() {
-    TCCR3A = 0x00;
-    TCCR3B = 0x00;
+    TCCR1A = 0x00;
+    TCCR1B = 0x00;
 }
 
 
@@ -164,7 +164,7 @@ int main(void) {
 	
 		
 	PWM_on1();
-	//PWM_on2();
+	PWM_on2();
 	PWM_on3();
 	PWM_on4();
     /* Insert your solution below */
