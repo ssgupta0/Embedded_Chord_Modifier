fbe9425e55947970e7f84d571866d2ede9a1a47b
Shashvat Gupta <45471982+ssgupta0@users.noreply.github.com>
2021-06-06 16:34:25 -0700

    Update pwm.h

--- header/pwm.h
+++ header/pwm.h
@@ -1,14 +1,14 @@
 //PB3
 void set_PWM0(double frequency) {
-    static double current_frequency;
+    static double current_frequency0;
 
-    if (frequency != current_frequency) {
+    if (frequency != current_frequency0) {
             TCCR0B |= 0x03;
 	    
             OCR0A = (short) (8000000 / (128/2 * frequency)) - 1;
         
         TCNT0 = 0;
-        current_frequency = frequency;
+        current_frequency0 = frequency;
    }
 }
 
@@ -27,15 +27,15 @@ void PWM_off0() {
 
 //PD7
 void set_PWM1(double frequency) {
-    static double current_frequency;
+    static double current_frequency1;
 
-    if (frequency != current_frequency) {
+    if (frequency != current_frequency1) {
             TCCR1B |= 0x03;
 	    
             OCR1A = (short) (8000000 / (128 * frequency)) - 1;
         
         TCNT1 = 0;
-        current_frequency = frequency;
+        current_frequency1 = frequency;
    }
 }
 
@@ -54,15 +54,15 @@ void PWM_off1() {
 
 //PD5
 void set_PWM2(double frequency) {
-    static double current_frequency;
+    static double current_frequency2;
 
-    if (frequency != current_frequency) {
+    if (frequency != current_frequency2) {
             TCCR2B |= 0x03;
 	    
             OCR2A = (short) (8000000 / (128/2 * frequency)) - 1;
         
         TCNT2 = 0;
-        current_frequency = frequency;
+        current_frequency2 = frequency;
    }
 }
 
@@ -81,15 +81,15 @@ void PWM_off2() {
 
 //PB6
 void set_PWM3(double frequency) {
-    static double current_frequency;
+    static double current_frequency3;
 
-    if (frequency != current_frequency) {
+    if (frequency != current_frequency3) {
             TCCR3B |= 0x03;
 	    
             OCR3A = (short) (8000000 / (128 * frequency)) - 1;
         
         TCNT3 = 0;
-        current_frequency = frequency;
+        current_frequency3 = frequency;
    }
 }
 
