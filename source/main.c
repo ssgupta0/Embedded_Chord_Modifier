f9dc43fe72249452a810cb5e8d15741841891ceb
Shashvat Gupta <45471982+ssgupta0@users.noreply.github.com>
2021-06-06 17:30:47 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -83,24 +83,7 @@ void setNotes(struct chords chord) {
 }
 
 /**/
-	struct notes n0;
-		n0.pos = 0;
-		n0.octave = 1;
-	struct notes n1;
-		n1.pos = 4;
-		n1.octave = 1;
-	struct notes n2;
-		n2.pos = 7;
-		n2.octave = 1;
-	struct notes n3;
-		n3.pos = 0;
-		n3.octave=n0.octave+1;
-	
-	struct chords chord;
-		chord.note0 = n0;
-		chord.note1 = n1;
-		chord.note2 = n2;
-		chord.note3 = n3;
+
 
 /**/
 
@@ -178,7 +161,24 @@ int main(void) {
 // 	initialize C2maj Chord.
 //  ------------------------
 
-
+	struct notes n0;
+		n0.pos = 0;
+		n0.octave = 1;
+	struct notes n1;
+		n1.pos = 4;
+		n1.octave = 1;
+	struct notes n2;
+		n2.pos = 7;
+		n2.octave = 1;
+	struct notes n3;
+		n3.pos = 0;
+		n3.octave=n0.octave+1;
+	
+	struct chords chord;
+		chord.note0 = n0;
+		chord.note1 = n1;
+		chord.note2 = n2;
+		chord.note3 = n3;
 	
 	setNotes(chord);
 	
