fec776e204ad02d3c27268cecb6dd121a32ab3ac
Shashvat Gupta <45471982+ssgupta0@users.noreply.github.com>
2021-06-06 23:27:30 -0700

    done

--- source/main.c
+++ source/main.c
@@ -4,6 +4,9 @@
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
+ *
+ *	GITHUB REPO: https://github.com/ssgupta0/Embedded_Chord_Modifier
+ *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
